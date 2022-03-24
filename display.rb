3981e53492c4d019b6d74a02b1d49203f2a86f35
Aaron <aaroncho10@gmail.com>
2022-03-24 01:21:58 -0400

    refactored #show_bases in Display to use ternary operator for cleaner code

--- display.rb
+++ display.rb
@@ -9,25 +9,12 @@ class Display
     def show_bases
         bases.each_with_index do |player, i|
             if i == 0
-                if player.is_a?(Hitter)
-                    puts "1st base: #{player.name}" 
-                else 
-                    puts "1st base: empty"
-                end
+                player.is_a?(Hitter) ? puts "1st base: #{player.name}" : puts "1st base: empty"
             elsif i == 1
-                if player.is_a?(Hitter)
-                    puts "2nd base: #{player.name}"
-                else
-                    puts "2nd base: empty"
-                end
+                player.is_a?(Hitter) ? puts "2nd base: #{player.name}" : puts "2nd base: empty"
             elsif i == 2
-                if player.is_a?(Hitter)
-                    puts "3rd base: #{player.name}"
-                else
-                    puts "3rd base: empty"
-                end
+                player.is_a?(Hitter) ? puts "3rd base: #{player.name}" : puts "3rd base: empty"
             end
         end
     end 
-
 end
